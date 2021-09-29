ee36ca88b420cebd8db4bb5e8a90d3a4f8881578
Gregory Holder <gregory.holder76@gmail.com>
2021-09-29 07:21:43 +0200

    Added a test code for the robot. It's purpose is to test the nRF modules and see if anything is actually being sent.

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,9 +1,24 @@
-//Robot.ino
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "test1";
 
 void setup() {
-	// put your setup code here, to run once:
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
 
 void loop() {
-	// put your main code here, to run repeatedly:
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+
+		Serial.print("Received message of length: ");
+		Serial.println(strlen(text));
+  }
 }
