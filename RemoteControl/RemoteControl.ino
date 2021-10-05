27ce0a5d3c46749114fe1e369274212e486b520e
blueted2 <gregory.holder76@gmail.com>
2021-10-05 19:00:16 +0200

    Took the nRF demo code and adapted/combined it with the previous version.

--- RemoteControl/RemoteControl.ino
+++ RemoteControl/RemoteControl.ino
@@ -1,148 +1,252 @@
-#include <SPI.h>
-#include <nRF24L01.h>
+//Transmitter.ino
+//#include <SPI.h>
 #include <RF24.h>
 
+#define IRQ_PIN		2
+#define CE_PIN		A0
+#define CSN_PIN		10
+
+#define LED_GREEN_PIN	A5
+
+#define RADIO_UPDATE_FREQUENCY_HZ	100
+#define RADIO_UPDATE_PERIOD_US		1000000 / RADIO_UPDATE_FREQUENCY_HZ
+#define LED_BLINK_HALF_PERIOD_MS	350
+
+#define TURBO_ACTIVE_DURATION_MS   3000
+#define TURBO_RECHARGE_DURATION_MS 3000
+
+#define MASK_DIR_RIGHT	0b00000001
+#define MASK_DIR_LEFT	  0b00000010
+#define MASK_TURBO		  0b00000100
+
+#define DIP_PIN_1 9
+#define DIP_PIN_2 8
+#define DIP_PIN_3 7
+#define DIP_PIN_4 6
+
+#define JOY_X_PIN A3
+#define JOY_Y_PIN A2
+#define ACCEL_PIN A4
+#define TURBO_PIN 4
+#define TURBO_PIN_2 5
+
+typedef struct {
+	uint8_t	right_speed;
+	uint8_t	left_speed;
+	uint8_t	bits_params;
+} packet_t;
+
 enum TurboState {
-  idle,
-  active,
-  recharging
+	disabled,
+	active,
+	recharging
 };
 
-// A struct containing all the data we want to send to the robot.
-typedef struct {
-  float left;
-  float right;
-  bool turbo;
-} RCData;
 
-// Input pin definitions
-#define PIN_JOY_X A3
-#define PIN_JOY_Y A2
-#define PIN_ACCEL A4
-#define PIN_TURBO 4
+// Instantiate an object for the nRF24L01 transceiver
+RF24 radio(CE_PIN, CSN_PIN);
 
-// nRF24L01 pins
-#define NRF_CE  A0
-#define NRF_CSN 10
+uint8_t address_transmitter[6] = "Rem00";
+uint8_t address_receiver[6] = "Rob00";
+uint8_t selected_channel;	// From 0 to 16
 
-// Fixed values
-#define DEADZONE 0.1f
-#define TURBO_ACTIVE_TIME 2000
-#define TURBO_RECHARGE_TIME 2000
+bool connected = false;
+bool led_state = false;
+uint32_t last_led_blink_ms;
 
-// Global variables
-float joyX = 0.0f;
-float joyY = 0.0f;
-float accl = 0.0f;
-bool turbo_btn_state = false;
+float right_speed;
+float left_speed;
+TurboState turbo_state = disabled;
+uint32_t turbo_active_start_ms;
+uint32_t turbo_recharge_start_ms;
 
-uint32_t turbo_active_start;
-uint32_t turbo_recharge_start;
-TurboState turbo_state = idle;
+volatile uint32_t last_radio_update_us;
 
+uint8_t get_dip_switches_selection();
+void interruptHandler();
 
-RF24 radio(NRF_CE, NRF_CSN);
+void setup() {
+	Serial.begin(115200);
 
-const byte address[6] = "test1";
+	// Initialize the transceiver on the SPI bus
+	if (!radio.begin()) {
+		Serial.println(F("Radio hardware is not responding!"));
+		while (1) {} // Hold in infinite loop; Maybe put a red led blinking
+	}
+  
+	pinMode(LED_GREEN_PIN, OUTPUT);
 
-// Function prototypes
-void update_input_values();
-void update_turbo_state();
-void send_motor_and_turbo_values();
+  // Initialize the DIP switch pins
+  pinMode(DIP_PIN_1, INPUT_PULLUP);
+  pinMode(DIP_PIN_2, INPUT_PULLUP);
+  pinMode(DIP_PIN_3, INPUT_PULLUP);
+  pinMode(DIP_PIN_4, INPUT_PULLUP);
 
-void setup(){
-  pinMode(PIN_TURBO, INPUT_PULLUP);
-  Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+	// Initialize the turbo button
+	// Here we only use one of them, but we could require the user to press both to activate the turbo.
+	pinMode(TURBO_PIN, INPUT_PULLUP);
+	pinMode(TURBO_PIN_2, INPUT_PULLUP);
+
+	// Configure interrupt
+	// pinMode(IRQ_PIN, INPUT);
+	// attachInterrupt(digitalPinToInterrupt(IRQ_PIN), interruptHandler, FALLING);
+
+	// Get selected channel from the dip switches
+	selected_channel = get_dip_switches_selection();
+
+	// 126 1MHz radio channels available: from 0 to 126 == 2400MHz to 2526MHz
+	radio.setChannel(80 + 3 * selected_channel);	// Range: 2480;2483;2486;2489;2492;...;2525 MHz
+	radio.setPALevel(RF24_PA_HIGH);
+	radio.setDataRate(RF24_250KBPS);
+	radio.setPayloadSize(sizeof(packet_t));
+
+	// Radio address = "Rem"+str(selected_channel) and "Rob"+str(selected_channel) (ex: "Rem07")
+	address_transmitter[4] = address_receiver[4] = '0' + (selected_channel / 10) % 10;		// Tens digit
+	address_transmitter[5] = address_receiver[5] = '0' + selected_channel % 10;			// Unit digit
+
+	// Open writing pipe on the receiver address
+	radio.openWritingPipe(address_receiver);	// always uses pipe 0
+
+	// Open reading pipe on the transmitter address
+	radio.openReadingPipe(1, address_transmitter);	// using pipe 1
 }
 
-void loop(){
-  update_input_values();
-  update_turbo_state();
-  send_motor_and_turbo_values();
-  delay(50);
-}
+void loop() {
+	// 1) Read inputs
+	/* SERIAL DEBUG */
+	// if (Serial.available()) {
+	// 	uint8_t cmd = Serial.read();
+	// 	switch (cmd) {
+	// 		case 'T':
+	// 			turbo_ON ^= 1;
+	// 			Serial.println(turbo_ON ? "Turbo ON !": "Turbo OFF");
+	// 			break;
+	// 		case 'R':
+	// 			right_speed = Serial.parseFloat();
+	// 			Serial.println(right_speed);
+	// 			break;
+	// 		case 'L':
+	// 			left_speed = Serial.parseFloat();
+	// 			Serial.println(left_speed);
+	// 			break;
+	// 		default:
+	// 			// Fault: empty Rx buffer
+	// 			while(Serial.available()) Serial.read();
+	// 	}
+	// }
+	/* END SERIAL DEBUG */
 
-// A version of "map()" for floats.
-float mapf(float x, float in_min, float in_max, float out_min, float out_max){
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-float remap_deadzone(float value, float deadzone){
-  if(value >  deadzone) return mapf(value,  deadzone, 1.0f, 0.0f, 1.0f);
-  if(value < -deadzone) return mapf(value, -1.0f, -deadzone, -1.0f, 0.0f);
-
-  return 0.0f;
-}
-
-// Maps the first range to the second range, with an added deadzone from -deadzone to +deadzone.
-float remap_with_deadzone(float value, float from_lower, float from_upper, float to_lower, float to_upper, float deadzone){
-  return remap_deadzone(mapf(value, from_lower, from_upper, to_lower, to_upper), deadzone);
-}
-
-void update_input_values(){
-  int rawJoyX = analogRead(PIN_JOY_X);
-  int rawJoyY = analogRead(PIN_JOY_Y);
-  int rawAccl = analogRead(PIN_ACCEL);
-
+  /* START READ JOYSTICKS */
+  int rawJoyX = analogRead(JOY_X_PIN);
+  int rawJoyY = analogRead(JOY_Y_PIN);
+  int rawAccl = analogRead(ACCEL_PIN);
 
   float joyX_unnormalized = remap_with_deadzone(rawJoyX, 1023, 0, -1, 1, 0.1f);
   float joyY_unnormalized = remap_with_deadzone(rawJoyY, 0, 1023, -1, 1, 0.1f);
 
   float joy_modulus = sqrtf(joyX_unnormalized * joyX_unnormalized + joyY_unnormalized * joyY_unnormalized);
 
-  joyX = joyX_unnormalized / joy_modulus;
-  joyY = joyY_unnormalized / joy_modulus;
-  accl = remap_with_deadzone(rawAccl, 1023, 0, 0, 1, 0.1f);
-  turbo_btn_state = digitalRead(PIN_TURBO) == LOW;
+  float joyX = joyX_unnormalized / joy_modulus;
+  float joyY = joyY_unnormalized / joy_modulus;
+	
+  float accl = remap_with_deadzone(rawAccl, 1023, 0, 0, 1, 0.1f);
+  bool turbo_btn_state = !digitalRead(TURBO_PIN) or !digitalRead(TURBO_PIN_2);
+
+  left_speed  = constrain( joyX + joyY, -1, 1) * accl;
+  right_speed = constrain(-joyX + joyY, -1, 1) * accl;
+  /* END READ JOYSTICKS */
+
+	// 2) Blink the connection status LED (green) if not connected
+	if (!connected && millis() - last_led_blink_ms > LED_BLINK_HALF_PERIOD_MS) {
+		last_led_blink_ms = millis();
+		digitalWrite(LED_GREEN_PIN, led_state ^= 1);
+	}
+
+	// 3) Determine if the turbo should be active, recharging or disabled
+	switch (turbo_state)
+	{
+	case disabled:
+		if(turbo_btn_state){
+			turbo_state = active;
+			turbo_active_start_ms = millis();
+		}
+		break;
+	case active:
+		if(millis() - turbo_active_start_ms > TURBO_ACTIVE_DURATION_MS){
+			turbo_state = recharging;
+			turbo_recharge_start_ms = millis(); // We could also do turbo_active_start_ms + TURBO_ACTIVE_DURATION
+		}
+		break;
+	case recharging:
+		if(millis() - turbo_recharge_start_ms > TURBO_RECHARGE_DURATION_MS){
+			turbo_state = disabled;
+		}
+		break;
+	default:
+		break;
+	}
+
+	// 4) Periodically send the speeds' informations through radio
+	if (micros() - last_radio_update_us > RADIO_UPDATE_PERIOD_US) {
+		last_radio_update_us = micros();
+
+		packet_t spd_data = {0, 0, 0};
+
+		if (right_speed >= 0)
+			spd_data.bits_params |= MASK_DIR_RIGHT;
+		if (left_speed >= 0)
+			spd_data.bits_params |= MASK_DIR_LEFT;
+		if (turbo_state == active)
+			spd_data.bits_params |= MASK_TURBO;
+
+		spd_data.right_speed = abs(right_speed) * 255;
+		spd_data.left_speed = abs(left_speed) * 255;
+
+		radio.write(&spd_data, sizeof(packet_t));
+		Serial.print("left: ");
+		Serial.print(left_speed);
+		Serial.print("; right: ");
+		Serial.println(right_speed);
+	}
 }
 
-// Updates the turbo state. Depends on the current state, how long the turbo has been in the state, and if the turbo button is being pressed.
-void update_turbo_state(){
-  switch(turbo_state){
-    case idle:
-      if(turbo_btn_state){
-        turbo_state = active;
-        turbo_active_start = millis();
-      }
-      break;
-
-    case active:
-      if(millis() - turbo_active_start > TURBO_ACTIVE_TIME){
-        turbo_state = recharging;
-        turbo_recharge_start = millis();
-      }
-      break;
-    
-    case recharging:
-      if(millis() - turbo_recharge_start > TURBO_RECHARGE_TIME){
-        turbo_state = idle;
-      }
-      break;
-  }
+uint8_t get_dip_switches_selection() {
+	uint8_t dip_value = 0;
+  dip_value |= digitalRead(DIP_PIN_1) << 0;
+  dip_value |= digitalRead(DIP_PIN_2) << 1;
+  dip_value |= digitalRead(DIP_PIN_3) << 2;
+  dip_value |= digitalRead(DIP_PIN_4) << 3;
+  Serial.println(dip_value);
+	return dip_value;
 }
 
-void send_motor_and_turbo_values(){
-  RCData rc_data;
+// void interruptHandler() {
+// 	bool tx_ds, tx_df, rx_dr;					// declare variables for IRQ masks
+// 	radio.whatHappened(tx_ds, tx_df, rx_dr);	// get values for IRQ masks
 
-  float left = constrain( joyX + joyY, -1, 1) * accl;
-  float right = constrain(-joyX + joyY, -1, 1) * accl;
-  
-  rc_data.left = left;
-  rc_data.right = right;
-  rc_data.turbo = (turbo_state == active);
+// 	if (tx_ds) {
+// 		connected = true;
+// 		// digitalWrite(LED_GREEN_PIN, HIGH);
+// 	}
+// 	if (tx_df)
+// 		connected = false;
+// }
 
+// A version of "map()" for floats.
+float mapf(float x, float in_min, float in_max, float out_min, float out_max){
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
-  // Create a union between the data we want to send and an array of bytes of the same length.
-  union {
-    RCData data;
-    byte bytes[sizeof(data)];
-  } u;
+// Maps the first range to the second range, with an added deadzone from -deadzone to +deadzone.
+float remap_with_deadzone(float value, float from_lower, float from_upper, float to_lower, float to_upper, float deadzone){
 
-  u.data = rc_data;
-  
-  radio.write(u.bytes, sizeof(u.bytes));
+  // Remap the the value from the first range to the second range.
+  float remapped_value = mapf(value, from_lower, from_upper, to_lower, to_upper);
+
+  // Remap the ranges on either side of the deadzone to start from 0 on the edge of the deadzone.
+  if(remapped_value >  deadzone) return mapf(remapped_value,  deadzone, 1.0f, 0.0f, 1.0f);
+  if(remapped_value < -deadzone) return mapf(remapped_value, -1.0f, -deadzone, -1.0f, 0.0f);
+
+  // If the value is within the deadzone, return 0
+  return 0.0f;
 }
--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,104 +1,285 @@
-#include <SPI.h>
-#include <nRF24L01.h>
+//Receiver.ino
+//#include <SPI.h>
 #include <RF24.h>
 
-#define LEFT_PWM_PIN      3
-#define LEFT_FORWARD_PIN  A4
-#define LEFT_BACKWARD_PIN A5
+#define IRQ_PIN		2
+#define CE_PIN		8
+#define CSN_PIN		10
 
-#define RIGHT_PWM_PIN      6
-#define RIGHT_FORWARD_PIN  5
-#define RIGHT_BACKWARD_PIN 4
+#define VBAT_PIN	A6
+#define LED_CONNECTION_PIN	7
+#define LED_TURBO_PIN   9
 
-// nRF24L01 pins
-#define NRF_CE  8
-#define NRF_CSN 10
+#define EN_G 3
+#define EN_D 6
+#define IN1  5
+#define IN2  4
+#define IN3  A4
+#define IN4  A5
 
-#define BAT_VOLT_PIN A6
+#define LED_BLINK_HALF_PERIOD_MS	350
+#define CONNECTION_TIMEOUT_MS		  200
+#define NOMINAL_MOTOR_V 6
 
-// A struct containing all the data we want to send to the robot.
+#define TURBO_LED_BLINK_HALF_PERIOD_MS 100
+
+#define TURBO_ACTIVE_DURATION_MS   2500 // Must be longer than the duration on the remote.
+#define TURBO_RECHARGE_DURATION_MS 2500 // Must be shorter than the duration on the remote.
+
+#define MASK_DIR_RIGHT	0b00000001
+#define MASK_DIR_LEFT	  0b00000010
+#define MASK_TURBO		  0b00000100
+#define SHIFT_DIR_RIGHT	0
+#define SHIFT_DIR_LEFT	1
+#define SHIFT_TURBO		2
+
+#define DIP_PIN_1 A3
+#define DIP_PIN_2 A2
+#define DIP_PIN_3 A1
+#define DIP_PIN_4 A0
+
 typedef struct {
-  float left;
-  float right;
-  bool turbo;
-} RCData;
+	uint8_t	right_speed;
+	uint8_t	left_speed;
+	uint8_t	bits_params;
+} packet_t;
 
-RF24 radio(NRF_CE, NRF_CSN); // CE, CSN
+enum TurboState {
+	disabled,
+	active,
+	recharging
+};
 
-RCData rc_data;
+// Instantiate an object for the nRF24L01 transceiver
+RF24 radio(CE_PIN, CSN_PIN);
 
-const byte address[6] = "test1";
+uint8_t address_transmitter[6] = "Rem00";
+uint8_t address_receiver[6] = "Rob00";
+uint8_t selected_channel;	// From 0 to 16
 
-byte buffer[32];
 
-float bat_volt;
+volatile uint8_t right_speed = 0;		// 0->255 <-> 0%->100%
+volatile uint8_t left_speed = 0;		// 0->255 <-> 0%->100%
+volatile bool right_dir_forward = true;	// true = forward; false = reverse
+volatile bool left_dir_forward = true;	// true = forward; false = reverse
+
+TurboState turbo_state = disabled;
+bool turbo_btn_state = false;
+
+uint32_t turbo_active_start_ms;
+uint32_t turbo_recharge_start_ms;
+
+volatile uint8_t current_pwm_right = 0;
+volatile uint8_t current_pwm_left = 0;
+
+volatile bool battery_low = false;
+float battery_low_threshold = 6.4;	// Battery cell is too low at 3.2V; 2S (2 cells) battery -> 6.4V
+
+volatile uint32_t last_packet_received;
+volatile bool connected = false;
+bool led_state = false;
+uint32_t last_led_blink_ms;
+
+uint8_t get_dip_switches_selection();
+float get_battery_voltage();
+uint8_t calc_pwm(uint8_t pwm_in, bool is_turning);
+void update_motors(uint8_t right_pwm, uint8_t left_pwm, bool right_forward, bool left_forward);
+void interruptHandler();
 
 void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+	Serial.begin(115200);
 
-  pinMode(LEFT_PWM_PIN, OUTPUT);
-  pinMode(LEFT_FORWARD_PIN, OUTPUT);
-  pinMode(LEFT_BACKWARD_PIN, OUTPUT);
+	// Initialize the transceiver on the SPI bus
+	if (!radio.begin()) {
+		Serial.println(F("Radio hardware is not responding!"));
+		while (1) {} // Hold in infinite loop; Maybe put a red led blinking
+	}
 
-  pinMode(RIGHT_PWM_PIN, OUTPUT);
-  pinMode(RIGHT_FORWARD_PIN, OUTPUT);
-  pinMode(RIGHT_BACKWARD_PIN, OUTPUT);
+	pinMode(LED_CONNECTION_PIN, OUTPUT);
+	pinMode(LED_TURBO_PIN, OUTPUT);
+	
+	pinMode(EN_G, OUTPUT);
+	pinMode(EN_D, OUTPUT);
+	pinMode(IN1, OUTPUT);
+	pinMode(IN2, OUTPUT);
+	pinMode(IN3, OUTPUT);
+	pinMode(IN4, OUTPUT);
 
-  digitalWrite(LEFT_FORWARD_PIN, HIGH);
-  digitalWrite(RIGHT_FORWARD_PIN, HIGH);
+	// Initialize the DIP switch pins
+  pinMode(DIP_PIN_1, INPUT_PULLUP);
+  pinMode(DIP_PIN_2, INPUT_PULLUP);
+  pinMode(DIP_PIN_3, INPUT_PULLUP);
+  pinMode(DIP_PIN_4, INPUT_PULLUP);
+
+	// Configure interrupt
+	pinMode(IRQ_PIN, INPUT);
+	attachInterrupt(digitalPinToInterrupt(IRQ_PIN), interruptHandler, FALLING);
+
+	// Get selected channel from the dip switches
+	selected_channel = get_dip_switches_selection();
+
+	// 126 1MHz radio channels available: from 0 to 126 == 2400MHz to 2526MHz
+	radio.setChannel(80 + 3 * selected_channel);	// Range: 2480;2483;2486;2489;2492;...;2525 MHz
+	radio.setPALevel(RF24_PA_HIGH);
+	radio.setDataRate(RF24_250KBPS);
+	radio.setPayloadSize(sizeof(packet_t));
+
+	// Radio address = "Rem"+str(selected_channel) and "Rob"+str(selected_channel) (ex: "Rem07")
+	address_transmitter[4] = address_receiver[4] = '0' + (selected_channel / 10) % 10;		// Tens digit
+	address_transmitter[5] = address_receiver[5] = '0' + selected_channel % 10;			// Unit digit
+
+	// Open writing pipe on the transmitter address
+	radio.openWritingPipe(address_transmitter);	// always uses pipe 0
+
+	// Open reading pipe on the receiver address
+	radio.openReadingPipe(1, address_receiver);	// using pipe 1
+
+	radio.startListening();
 }
 
 void loop() {
-  read_battery_voltage();
-  update_rc_data();
-  update_motor_outputs();
-  delay(10);
+	// 1) Check voltage (if below a threshold (like 6.4V), put battery_low to true and stop the motors)
+
+
+	// 2) Check connection timeout and blink the connection status LED (green) if not connected
+	if (!connected) {
+		if (millis() - last_led_blink_ms > LED_BLINK_HALF_PERIOD_MS) {
+			last_led_blink_ms = millis();
+			digitalWrite(LED_CONNECTION_PIN, led_state ^= 1);
+		}
+	}
+	else if (millis() - last_packet_received > CONNECTION_TIMEOUT_MS) {
+		connected = false;
+		update_motors(0, 0, true, true);
+	}
+
+	// 3) Check turbo ON time and update the turbo LED
+	switch (turbo_state)
+	{
+	case disabled:
+		digitalWrite(LED_TURBO_PIN, HIGH);
+
+		if(turbo_btn_state){
+			turbo_state = active;
+			turbo_active_start_ms = millis();
+		}
+		break;
+	
+	case active:
+		digitalWrite(LED_TURBO_PIN, (millis() - turbo_recharge_start_ms) % (2*TURBO_LED_BLINK_HALF_PERIOD_MS) > TURBO_LED_BLINK_HALF_PERIOD_MS);
+
+		// If the turbo has been active long enough, or the remote is no longer requesing turbo
+		if(millis() > turbo_active_start_ms + TURBO_ACTIVE_DURATION_MS || !turbo_btn_state){
+			turbo_state = recharging;
+			turbo_recharge_start_ms = millis();
+		}
+		break;
+
+	case recharging:
+		// analogWrite(LED_TURBO_PIN, (millis()-turbo_recharge_start_ms) * 255 / TURBO_RECHARGE_DURATION_MS);
+		digitalWrite(LED_TURBO_PIN, LOW);
+		if(millis() > turbo_recharge_start_ms +  TURBO_RECHARGE_DURATION_MS){
+			turbo_state = disabled;
+		}
+		break;
+	}
+
+	/* SERIAL DEBUG */
+	static uint32_t last_print_ms;
+	if (millis() - last_print_ms > 100) {
+		last_print_ms = millis();
+
+		if (!right_dir_forward) Serial.write('-');
+		Serial.println(current_pwm_left);
+		if (!left_dir_forward) Serial.write('-');
+		Serial.println(current_pwm_right);
+		Serial.println(turbo_state == active ? "Turbo ON !": "Turbo OFF");
+	}
+	/* END SERIAL DEBUG */
 }
 
-void update_rc_data(){
-  union {
-    RCData data;
-    byte bytes[32];
-  } u;
 
-  if (radio.available()) {
-    radio.read(u.bytes, sizeof(u.bytes));
-    
-    rc_data = u.data;
-  }
+uint8_t get_dip_switches_selection() {
+	uint8_t dip_value = 0;
+  dip_value |= digitalRead(DIP_PIN_1) << 0;
+  dip_value |= digitalRead(DIP_PIN_2) << 1;
+  dip_value |= digitalRead(DIP_PIN_3) << 2;
+  dip_value |= digitalRead(DIP_PIN_4) << 3;
+  Serial.println(dip_value);
+	return dip_value;
 }
 
-// A version of "map()" for floats.
-float mapf(float x, float in_min, float in_max, float out_min, float out_max){
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+float get_battery_voltage() {
+	// Voltage dividor: 20kohm & 10kohm => volatge divided by 3
+	// raw_value: 0 -> 1023
+	// measured_voltage = raw_value / 1023 * 5: 0 -> 5V
+	// real_volatge = measured_voltage * 3: 0 -> 15V
+	uint16_t raw_value = analogRead(VBAT_PIN);
+	return (float) raw_value * 3.0 * 5.0 / 1023.0;
 }
 
-void update_motor_outputs(){
+uint8_t calc_pwm(uint8_t pwm_in, bool is_turning) {
+	// Return PWM ajusted to the battery voltage
+	// - nominal motor voltage is 6V
+	// - fully charged battery gives 8.4V
+	
+	// if(is_turning) return pwm_in;
+	if(turbo_state == active) return pwm_in;
 
-  float output_coef = 6/bat_volt;
-  if(output_coef > 1) output_coef = 1.0f;
+	float bat_volt = get_battery_voltage();
+	float pwm_coef = constrain(NOMINAL_MOTOR_V / bat_volt, 0, 1);
+	return pwm_in * pwm_coef;
 
-
-  digitalWrite(LEFT_BACKWARD_PIN, rc_data.left < 0);
-  digitalWrite(LEFT_FORWARD_PIN, rc_data.left > 0);
-
-  digitalWrite(RIGHT_BACKWARD_PIN, rc_data.right < 0);
-  digitalWrite(RIGHT_FORWARD_PIN, rc_data.right > 0);
-
-  analogWrite(RIGHT_PWM_PIN, abs(rc_data.right * 255 * output_coef));
-  analogWrite(LEFT_PWM_PIN, abs(rc_data.left * 255 * output_coef));
-
-  Serial.print("left: ");
-  Serial.print(rc_data.left);
-  Serial.print("; right: ");
-  Serial.println(rc_data.right);
+	// Maybe adjust the fact that the final speed of a motor is not linear (proportional) to its PWM
+	// Or later
 }
 
-void read_battery_voltage(){
-  int bat_pin_in = analogRead(BAT_VOLT_PIN);
-  bat_volt = mapf(bat_pin_in, 0.0f, 1023.0f, 0.0f, 15.0f);
+void update_motors(uint8_t right_pwm, uint8_t left_pwm, bool right_forward, bool left_forward) {
+	// Apply pwm and direction on both L293D
+	if (right_pwm != current_pwm_right) {
+		current_pwm_right = right_pwm;
+		analogWrite(EN_D, right_pwm);
+		digitalWrite(IN1, right_forward);
+		digitalWrite(IN2, !right_forward);
+	}
+	if (left_pwm != current_pwm_left) {
+		current_pwm_left = left_pwm;
+		analogWrite(EN_G, left_pwm);
+		digitalWrite(IN3, !left_forward);
+		digitalWrite(IN4, left_forward);
+	}
+
+	return;
+}
+
+void interruptHandler() {
+	bool tx_ds, tx_df, rx_dr;					// declare variables for IRQ masks
+	radio.whatHappened(tx_ds, tx_df, rx_dr);	// get values for IRQ masks
+	// whatHappened() clears the IRQ masks also. This is required for
+	// continued TX operations when a transmission fails.
+	// clearing the IRQ masks resets the IRQ pin to its inactive state (HIGH)
+
+	if (rx_dr) {
+		packet_t rcv_data;
+		while (radio.available()) {
+			radio.read(&rcv_data, sizeof(packet_t));
+		}
+
+		right_speed = rcv_data.right_speed;
+		left_speed = rcv_data.left_speed;
+		right_dir_forward = (rcv_data.bits_params & MASK_DIR_RIGHT) >> SHIFT_DIR_RIGHT;
+		left_dir_forward = (rcv_data.bits_params & MASK_DIR_LEFT) >> SHIFT_DIR_LEFT;
+		turbo_btn_state = (rcv_data.bits_params & MASK_TURBO) >> SHIFT_TURBO;
+
+		// Check battery low
+		// if (! battery_low)
+		bool is_turning = (right_dir_forward != left_dir_forward);
+		update_motors(calc_pwm(right_speed, is_turning), calc_pwm(left_speed, is_turning), right_dir_forward, left_dir_forward);
+
+		last_packet_received = millis();
+		connected = true;
+		digitalWrite(LED_CONNECTION_PIN, HIGH);
+	}
+
+	return;
 }
